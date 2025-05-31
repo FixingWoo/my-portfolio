10d4f340ac8395e1d1fdd391ca469ad2689b53d5
FixingWoo <fixingwoo@gmail.com>
2025-05-31 22:38:52 +0900

    feat: Mask & Background

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.mask}>
+      <div className={styles.background}>
+        <main></main>
+      </div>
+    </div>
+  );
+}
