24cdbc8098cf90ac0dbe5f58780fc47269db6016
FixingWoo <fixingwoo@gmail.com>
2025-06-11 17:46:42 +0900

    feat: Blog 메세지

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,13 @@
+import Column from '@/components/Column';
+import Flex from '@/components/Flex';
+import Heading from '@/components/Heading';
+
 export default function Blog() {
-  return <>Blog</>;
+  return (
+    <Column maxWidth={'m'}>
+      <Flex fillWidth horizontal="center" paddingTop="48">
+        <Heading variant="display-strong-m">Coming Soon</Heading>
+      </Flex>
+    </Column>
+  );
 }
