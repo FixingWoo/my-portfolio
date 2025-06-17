f8d4ab5e9aac7097d57af7064309d414e50cf333
FixingWoo <fixingwoo@gmail.com>
2025-06-17 13:42:32 +0900

    style: 미사용 리소스 제거

--- src/resources/config.ts
+++ src/resources/config.ts
@@ -1,16 +1,12 @@
 const baseURL = 'https://www.fixingwoo.com';
 
 const style = {
-  theme: 'dark', // theme is not necessary when using ThemeProvider (default)
-  neutral: 'gray', // sand | gray | slate
-  brand: 'cyan', // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: 'red', // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  solid: 'contrast', // color | contrast
-  solidStyle: 'flat', // flat | plastic
-  border: 'playful', // rounded | playful | conservative
-  surface: 'translucent', // filled | translucent
+  theme: 'light',
+  neutral: 'gray',
+  solid: 'contrast',
+  border: 'rounded',
+  surface: 'translucent',
   transition: 'all', // all | micro | macro
-  scaling: '100', // 90 | 95 | 100 | 105 | 110
 };
 
 const routes = {
