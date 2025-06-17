9f29d466c863182f756d465302da79ecf6a8dcc7
FixingWoo <fixingwoo@gmail.com>
2025-06-17 13:50:49 +0900

    feat: 폰트 적용

--- src/resources/config.ts
+++ src/resources/config.ts
@@ -16,4 +16,18 @@ const routes = {
   '/blog': true,
 };
 
-export { baseURL, style, routes };
+import { Noto_Sans_KR } from 'next/font/google';
+
+const primaryFont = Noto_Sans_KR({
+  variable: '--font-primary',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const font = {
+  primary: primaryFont,
+  secondary: primaryFont,
+  tertiary: primaryFont,
+};
+
+export { baseURL, style, routes, font };
